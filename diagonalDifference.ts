954f2e6f80f44077834afe11535f90017dc41785
isampedro98 <101359566+isampedro98@users.noreply.github.com>
2024-11-08 22:05:45 -0300

    Create diagonalDifference.ts

---
+++ diagonalDifference.ts
@@ -0,0 +1,57 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ */
+
+function diagonalDifference(arr: number[][]): number {
+    // Write your code here
+    let left = 0, right = 0;
+    for( let i = 0; i < arr.length; i++ ) {
+        left += arr[i][i];
+        right += arr[arr.length-i-1][i];
+    }
+    return Math.abs(left-right);
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    let arr: number[][] = Array(n);
+
+    for (let i: number = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result: number = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
